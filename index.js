db968f469bcf6ede7ee55270a96631529a84a076
Tommy Lin <tommylin121314@gmail.com>
2022-06-02 16:43:05 -0400

    Implemented add player functionality up to four players.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+num_players = 2;
+
 function rollDice() {
     var player_1_score = Math.floor(Math.random() * 6) + 1;
     var player_2_score = Math.floor(Math.random() * 6) + 1;
@@ -16,5 +18,23 @@ function rollDice() {
     }
     else {
         prompt.textContent = "Player 2 Wins.";
+    }
+}
+
+function addPlayer() {
+    console.log("add player");
+    if(num_players < 4) {
+        num_players++;
+        var add_player = document.getElementsByClassName("add-player")[0];
+        var players = document.getElementsByClassName("players")[0];
+
+        var new_player = document.createElement("div");
+        new_player.classList.add("player")
+        new_player.innerHTML = "<h2 class='player-title'>Player " + num_players + "</h2><img class='die-image' src='images/dice1.png'>";
+        players.insertBefore(new_player, add_player)
+
+        if(num_players == 4) {
+            players.removeChild(add_player);
+        }
     }
 }
